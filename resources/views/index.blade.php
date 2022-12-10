9c0e06c20dce5bfbcd1a8d8db45f075aa7699fc4
suguru-gbgg <takagisn12@gmail.com>
2022-12-10 16:35:03 +0900

    jquery適用

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<input type="text">
+<input type="text" class="text" value="">
+<button type="button">送信</button>
+<button id="button">ボタン</button>
 @endsection
