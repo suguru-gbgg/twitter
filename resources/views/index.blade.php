84b6101b91794bea355ed66e570a25df7dc9c9b5
suguru-gbgg <takagisn12@gmail.com>
2023-02-03 00:19:01 +0900

    カードでタイムラインを作った

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,31 +16,25 @@
 <form action="MainController" method="post">
 @csrf
   
-    <ul class="parent">
-    <?php $i=0; ?>
-      @foreach ($dbtexts as $dbtext)
-        <div class="card" style="width: 18rem;">
-          <div class="card-body">
-            <p class="card-text">
-              <li>
-                <?php echo $dbtext->text; ?>
-                <input type="hidden" value="{{ $dbtext->text }}" class="text1" name="text{{ $i }}">
-              </li>
-            </p>
+    <ul class="parent container">
+      <div class="row flex-row">
+      <?php $i=0; ?>
+        @foreach ($dbtexts as $dbtext)
+        <div class="col-3">
+          <div class="card mt-4"> 
+            <div class="card-body">
+              <h5 class="card-title">{{ $dbtext->text; }}</h5>
+              <p class="card-text">{{ $dbtext->user_name; }}</p>
+              <button type="submit" class="btn btn-secondary">詳細</button>
+              <button type="submit" class="btn btn-danger">削除</button>
+            </div>
+            </div>
           </div>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item">ユーザー名 : {{ $dbtext->user_name }}</li>
-          </ul>
-          <div class="card-body">
-            <button type="submit" class="btn btn-secondary">詳細</button>
-            <button type="submit" class="btn btn-danger">削除</button>
-          </div>
-        </div>
-      <?php $i++ ; ?>    
-      @endforeach
-      
+          <?php $i++ ; ?>    
+        @endforeach
+      </div> 
     </ul>
-  
+    
   <button type="submit" class="btn btn-primary">保存</button>
 </form>
 </div>
