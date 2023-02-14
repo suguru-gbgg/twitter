8124a0a76771d43311faa0d8898176511fb3c3ac
suguru-gbgg <takagisn12@gmail.com>
2023-02-14 20:10:47 +0900

    見た目にかなりこだわった

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,40 +2,40 @@
 
 @section('content')
 <div class="container">
-  <div class="row">
-      <div class="col-sm"></div>
-      <div class="col-sm">
-          <input type="text" id="text" class="text-center" value="" name="text" placeholder="なにか入力してください">
-          <button type="button" id="addbtn" class="btn btn-primary">追加</button>
-      </div>
-      <div class="col-sm"></div>     
-  </div>
+  <ul class="parent">
+    <div class="mx-auto"  style="width: 20rem">
+      <div class="card">
+        <div class="card-body text-center">
+          <p class="card-text">
+            <textarea id="text" cols="33" rows="5" class="text-center" value="" name="text"></textarea>
+          </p>
+          <button type="button" id="addbtn" class="btn btn-outline-primary text-center">ツイート</a>
+        </div>
+    </div>
+  </ul>
 </div>
 
-<div class="mx-auto">
 <form action="MainController" method="post">
 @csrf
-  
-    <ul class="parent container">
-      <div class="row flex-row">
-      <?php $i=0; ?>
-        @foreach ($dbtexts as $dbtext)
-        <div class="col-3">
-          <div class="card mt-4"> 
-            <div class="card-body">
-              <h5 class="card-title">{{ $dbtext->text; }}</h5>
-              <p class="card-text">{{ $dbtext->user_name; }}</p>
-              <button type="submit" class="btn btn-secondary">詳細</button>
-              <button type="submit" class="btn btn-danger">削除</button>
+  <ul class="parent">
+    <?php $i=0; ?>
+    @foreach ($dbtexts as $dbtext)
+      <div class="card my-3 mx-auto" style="width: 20rem;">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item text-center">{{ $dbtext->user_name }}</li>
+          <li class="list-group-item text-center">{{ $dbtext->text }}
+            <div class="dropdown">
+              <h7 class="dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"></h7>
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li><a class="dropdown-item" href="#">詳細</a></li>
+                <li><a class="dropdown-item" href="#">削除</a></li>
+              </ul>
             </div>
-            </div>
-          </div>
-          <?php $i++ ; ?>    
-        @endforeach
-      </div> 
-    </ul>
-    
-  <button type="submit" class="btn btn-primary">保存</button>
+          </li>
+        </ul>
+      </div>
+    @endforeach 
+  </ul>
 </form>
 </div>
 @endsection
